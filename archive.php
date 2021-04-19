85a6f4e5165a0e7dd892b9ece27d0bc201e2ea62
Vincent <darkpaper28@gmail.com>
2021-04-19 13:29:48 -0400

    exercice 3

---
+++ archive.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme4w4
+ */
+
+get_header();
+?>
+///////////////////////////////////////////////// ARCHIVE.PHP
+	<main id="primary" class="site-main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+        <section class="galerie">
+
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				 ?>
+				 <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <?php the_post_thumbnail('thumbnail');  ?>
+    </div>
+    <div class="flip-card-back">
+      <?php get_template_part( 'template-parts/content', 'cours-Projets' ); ?>
+    </div>
+  </div>
+</div>
+
+			<!-- <div>
+				<?php /* the_post_thumbnail('thumbnail'); */ /* get_template_part( 'template-parts/content', 'cours-Projets' ); */ ?>
+				<div class="info">
+					<h2></h2>
+					<p></p>
+				</div>
+				</div> -->
+
+			<?php endwhile; ?>
+			</section>
+		<?php endif;?>
+		
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
